46fbeaf6dcc1fc73ca90a6f3443b39c9cfbe64b1
Manassarn "Noom" Manoonchai <manassarn@gmail.com>
2020-06-04 01:46:58 +0000

    Add button with counter

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+Vue.component("button-counter", {
+  data: () => {
+    return { count: 0 }
+  },
+  template: '<button @click="count++">{{ count }} 👏 claps</button>'
+})
+
 let app = new Vue({
   el: "#app",
   data: {
